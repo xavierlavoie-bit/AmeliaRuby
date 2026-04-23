23e59a06eb64085e87277bc3b937e332a64600a7
xavierlavoie-bit <xavier.lavoie@optimiplex.com>
2026-04-22 20:54:46 -0400

    "amelioration"

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Optionnel : garde ceci si tu avais configuré les images Unsplash
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
